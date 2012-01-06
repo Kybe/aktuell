063ee3ffb89e48b1282829cd21848ca7f026d266
Kybe <suz_holm@gmx.de>
2012-01-06 23:01:51 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aktl::Application.config.secret_token = 'a6319046beee29fd7188e92abf08d0b22405cb4f70460424306377c3dfbb7f59a1b5910388cb79b4dad9b0fd5f0f14959a296786675defe14dda9bef0546877d'
